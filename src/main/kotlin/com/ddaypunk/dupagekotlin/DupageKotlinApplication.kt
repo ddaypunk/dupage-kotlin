a82bf876f8870de02a76f2215c64a4164521945f
ddaypunk <andy@delso.me>
2020-10-14 15:23:06 -0500

    intial Spring project setup

---
+++ src/main/kotlin/com/ddaypunk/dupagekotlin/DupageKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.ddaypunk.dupagekotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DupageKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<DupageKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/com/ddaypunk/dupagekotlin/DupageKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ddaypunk.dupagekotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DupageKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
