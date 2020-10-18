db6146be073ddf52ce336ee02382fd75c046016f
ddaypunk <andy@delso.me>
2020-10-17 22:20:24 -0500

    add selenium dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,11 +21,13 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("org.seleniumhq.selenium:selenium-java:4.0.0-alpha-6")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	testImplementation("org.springframework.boot:spring-boot-starter-test") {
 		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
 	}
 	testImplementation("org.testcontainers:junit-jupiter")
+	testImplementation("io.github.bonigarcia:webdrivermanager:4.2.2")
 }
 
 dependencyManagement {
