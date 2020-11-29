1bcbdd4acf90135243a3e22910db5b14d5c8ab54
ddaypunk <dday@ddaypunk.com>
2020-11-28 20:08:32 -0600

    Use Firefox for now as default test browser

--- src/main/kotlin/com/ddaypunk/dupagekotlin/Driver.kt
+++ src/main/kotlin/com/ddaypunk/dupagekotlin/Driver.kt
@@ -5,19 +5,21 @@ import org.openqa.selenium.Dimension
 import org.openqa.selenium.Point
 import org.openqa.selenium.WebDriver
 import org.openqa.selenium.chrome.ChromeDriver
+import org.openqa.selenium.chrome.ChromeOptions
+import org.openqa.selenium.firefox.FirefoxDriver
 import java.net.URI
 import java.util.concurrent.TimeUnit
 
 object Driver {
     var driver: WebDriver
+    private val isLocal = PropManager.getProp("local").toBoolean()
+    private val browser = PropManager.getProp("browser")
 
     init {
         driver = createDriver()
     }
 
     private fun createDriver(): WebDriver {
-        val isLocal = PropManager.getProp("local").toBoolean()
-
         return if (isLocal) {
             getLocalDriver()
         } else {
@@ -26,7 +28,11 @@ object Driver {
     }
 
     private fun getLocalDriver(): WebDriver {
-        return getChromeDriver()
+        return when(browser.toLowerCase()) {
+            "chrome" -> getChromeDriver() // not working correctly on linux
+            "firefox" -> getFirefoxDriver() // works on linux
+            else -> getFirefoxDriver() // default to Firefox for now
+        }
     }
 
     private fun getRemoteDriver(): WebDriver {
@@ -35,12 +41,30 @@ object Driver {
 
     private fun getChromeDriver(): ChromeDriver {
         WebDriverManager.chromedriver().setup()
-        driver = ChromeDriver()
+        driver = ChromeDriver(getChromeOptions())
         driver.manage()?.timeouts()?.implicitlyWait(10, TimeUnit.SECONDS)
         driver.manage()?.window()?.position = Point(0, 0)
         driver.manage()?.window()?.size = Dimension(1280, 1080)
-        driver.get(URI(PropManager.getProp("host")).toString())
 
         return driver as ChromeDriver
+    }
+
+    private fun getFirefoxDriver(): FirefoxDriver {
+        WebDriverManager.firefoxdriver().setup()
+        driver = FirefoxDriver()
+
+        return driver as FirefoxDriver
+    }
+
+    private fun getChromeOptions(): ChromeOptions {
+        val ops = ChromeOptions()
+//        ops.addArguments("--disable-infobars") // disabling infobars
+//        ops.addArguments("--disable-extensions") // disabling extensions
+//        ops.addArguments("--disable-gpu") // applicable to windows os only
+        ops.addArguments("--headless")
+        ops.addArguments("--no-sandbox")
+        ops.addArguments("--disable-dev-shm-usage")
+
+        return ops
     }
 }
--- src/test/kotlin/com/ddaypunk/dupagekotlin/BaseTest.kt
+++ src/test/kotlin/com/ddaypunk/dupagekotlin/BaseTest.kt
@@ -1,26 +1,14 @@
 package com.ddaypunk.dupagekotlin
 
-import io.github.bonigarcia.wdm.WebDriverManager
 import org.junit.After
 import org.junit.Before
-import org.openqa.selenium.Dimension
-import org.openqa.selenium.Point
-import org.openqa.selenium.WebDriver
-import org.openqa.selenium.chrome.ChromeDriver
 import java.net.URI
-import java.util.concurrent.TimeUnit
 
 abstract class BaseTest {
-    lateinit var driver: WebDriver
-        private set
+     val driver = Driver.driver
 
     @Before
     fun setup() {
-        WebDriverManager.chromedriver().setup()
-        driver = ChromeDriver()
-        driver.manage()?.timeouts()?.implicitlyWait(10, TimeUnit.SECONDS)
-        driver.manage()?.window()?.position = Point(0, 0)
-        driver.manage()?.window()?.size = Dimension(1280, 1080)
         driver.get(URI(PropManager.getProp("host")).toString())
     }
 
